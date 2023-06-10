75ab7103970dccefb690d419373f75d18a08e465
RDW <duckwhale@mail.ru>
2023-06-10 18:14:17 +0200

    Deps: Add an MSYS-compatible build script for wgpu-native
    Not sure if there is something else to be done, but it does create a DLL...

---
+++ deps/wgpu-windowsbuild.sh
@@ -0,0 +1,8 @@
+set -e
+
+echo Building target wgpu-native
+
+make -C deps/gfx-rs/wgpu-native
+
+cp deps/gfx-rs/wgpu-native/target/release/wgpu_native.dll $(pwd)
+cp deps/gfx-rs/wgpu-native/ffi/webgpu-headers/webgpu.h $(pwd)
