6db7ccbbed1b165161b483133fb0ea4bbb6b0e56
RDW <duckwhale@mail.ru>
2023-06-11 09:28:16 +0200

    Update 04-draw-solid-color.lua

--- examples/04-draw-solid-color.lua
+++ examples/04-draw-solid-color.lua
@@ -8,3 +8,14 @@ local adapter = gpu.request_adapter_for_window_surface(context, window)
 local deviceInfo = gpu.request_device_for_adapter(adapter)
 
 local chain = gpu.create_swap_chain_for_window_surface(context, window, deviceInfo.device, adapter)
+
+-- Can now run the GLFW UI loop, either manually or with a polling timer (requires libuv)
+local success, uv = pcall(require, "uv")
+local isAsyncRuntime = success and (type(uv) == "table")
+if isAsyncRuntime then
+	print("Starting UI loop with a polling timer (non-blocking) ...")
+	gpu.run_ui_loop_with_libuv(window, deviceInfo.device, chain)
+else
+	print("Starting UI loop with GLFW (blocking) ...")
+	gpu.run_ui_loop_with_glfw(window, deviceInfo.device, chain)
+end
