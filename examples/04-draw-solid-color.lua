9dadd570f731f1ebe718c1b7f3a008167d796fa7
RDW <duckwhale@mail.ru>
2023-06-11 09:01:13 +0200

    Update 04-draw-solid-color.lua

--- examples/04-draw-solid-color.lua
+++ examples/04-draw-solid-color.lua
@@ -6,4 +6,5 @@ local window = gpu.create_gltf_window()
 local adapter = gpu.request_adapter_for_window_surface(context, window)
 
 local deviceInfo = gpu.request_device_for_adapter(adapter)
+
 local chain = gpu.create_swap_chain_for_window_surface(context, window, deviceInfo.device, adapter)
