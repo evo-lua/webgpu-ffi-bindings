6263450ab08b907a40cac9722abdd3ce08b7adcb
RDW <duckwhale@mail.ru>
2023-06-11 04:05:39 +0200

    Update 00-glfw-window-setup.lua

--- examples/00-glfw-window-setup.lua
+++ examples/00-glfw-window-setup.lua
@@ -1,3 +1,18 @@
 -- This just sets up a native window with a WebGPU-compatible surface to draw on
 local gpu = require("gpu")
-gpu.initialize()
+local context = gpu.initialize_webgpu_context()
+local window = gpu.create_gltf_window()
+local adapter = gpu.request_adapter_for_window_surface(context, window)
+
+assert(adapter, "Failed to create a GPU adapter for the given window's backing store")
+
+-- Can now run the GLFW UI loop, either manually or with a polling timer (requires libuv)
+local success, uv = pcall(require, "uv")
+local isAsyncRuntime = success and (type(uv) == "table")
+if isAsyncRuntime then
+	print("Starting UI loop with a polling timer (non-blocking)")
+	gpu.run_ui_loop_with_libuv(window)
+else
+	print("Starting UI loop with GLFW (blocking)")
+	gpu.run_ui_loop_with_glfw(window)
+end
