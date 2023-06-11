c6bf6a9e3df4e14e73cf7a1998bc389e6e45bf1a
RDW <duckwhale@mail.ru>
2023-06-11 05:12:40 +0200

    Update 02-logical-device-creation.lua

--- examples/02-logical-device-creation.lua
+++ examples/02-logical-device-creation.lua
@@ -1,1 +1,17 @@
 -- Based on https://eliemichel.github.io/LearnWebGPU/getting-started/the-device.html
+local gpu = require("gpu")
+
+local context = gpu.initialize_webgpu_context()
+local window = gpu.create_gltf_window()
+local adapter = gpu.request_adapter_for_window_surface(context, window)
+
+local options = {
+	-- Lua mapping of the fields contained in WGPUDeviceDescriptor (for ease of use)
+	-- Leave fields empty (or omit the options parameter entirely) to use default options
+}
+local deviceInfo = gpu.request_device_for_adapter(adapter, options)
+
+assert(deviceInfo.device, "Failed to create logical WebGPU device")
+assert(deviceInfo.descriptor, "Failed to create device descriptor for the provided creation options")
+
+print("Successfully created WebGPU device: " .. deviceInfo.options.label)
