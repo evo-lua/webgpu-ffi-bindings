ce0c8fbd9daedfd07a442779b6c2d99e87146bdb
RDW <duckwhale@mail.ru>
2023-06-11 08:55:54 +0200

    Update 04-draw-solid-color.lua

--- examples/04-draw-solid-color.lua
+++ examples/04-draw-solid-color.lua
@@ -1,1 +1,9 @@
 -- Based on https://eliemichel.github.io/LearnWebGPU/getting-started/first-color.html
+local gpu = require("gpu")
+
+local context = gpu.initialize_webgpu_context()
+local window = gpu.create_gltf_window()
+local adapter = gpu.request_adapter_for_window_surface(context, window)
+
+local deviceInfo = gpu.request_device_for_adapter(adapter)
+local chain = gpu.create_swap_chain_for_window_surface(context, window, deviceInfo.device, adapter)
